787f6c354f3893f67de33a9a6527b5a5d6137258
eddie <eddie@kang.ccnmtl.columbia.edu>
2011-09-29 11:11:24 -0400

    unescaping the questions

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-careermapblock",
-    version="0.2.8",
+    version="0.2.9",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
