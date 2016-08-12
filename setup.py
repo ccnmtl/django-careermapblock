b497c2feb3d950a507580802cee793cb6ac26ee9
Anders Pearson <anders@columbia.edu>
2016-08-12 11:08:54 +0100

    :bookmark: release 0.4.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-careermapblock",
-    version="0.3.1",
+    version="0.4.0",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
